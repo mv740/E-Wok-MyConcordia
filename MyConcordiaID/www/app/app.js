e824db91afec31314ffdceb9126f23f77abce391
francisct <franciscotetremblay@gmail.com>
2016-10-11 17:25:15 -0400

    #9: added sidenav

--- MyConcordiaID/www/app/app.js
+++ MyConcordiaID/www/app/app.js
@@ -3,6 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngAnimate',
+  'ngMaterial',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
@@ -11,4 +13,14 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}]).
+controller('MainCtrl', function($scope, $mdSidenav) {
+  $scope.openNav = function() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  /* Set the width of the side navigation to 0 */
+  $scope.closeNav = function() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+});
