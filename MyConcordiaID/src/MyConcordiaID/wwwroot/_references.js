d2c489cf3729b9ec42e5bf4bfe1757db2a8b9467
daBulgarian <harrison1993@gmail.com>
2016-10-26 17:18:58 -0400

    #9 Removed references to site.js, removed site.js, removed Close label for hamburger button toggle.

--- MyConcordiaID/src/MyConcordiaID/wwwroot/_references.js
+++ MyConcordiaID/src/MyConcordiaID/wwwroot/_references.js
@@ -5,7 +5,6 @@
 /// <reference path="js/searchBox.js" />
 /// <reference path="js/searchModal.js" />
 /// <reference path="js/searchresults.js" />
-/// <reference path="js/site.js" />
 /// <reference path="js/view1.js" />
 /// <reference path="lib/angular/angular.js" />
 /// <reference path="lib/angular-animate/angular-animate.js" />
