d8080d59436d677ed8ec83d573f59eb228328262
francisct <franciscotetremblay@gmail.com>
2016-10-28 08:55:07 -0400

    #9 added search request mock

---
+++ MyConcordiaID/src/MyConcordiaID/wwwroot/mocks/searchRequestMock.js
@@ -0,0 +1,11 @@
+﻿{
+    "firstName": "Francis",
+    "lastName": "Cote-Tremblay",
+    "id": "9999999",
+    "birthdate": {
+        "day": "10",
+        "month": "april",
+        "year": "1992"
+    },
+    "netname": "f_cotetr"
+}
