19a026382c54acb3b64289147f24c73a3a30beb7
Sebastian Shah - EXPSPACE <sebshah@hotmail.com>
2016-11-05 14:53:11 -0400

    #16 Get request for student information + displaying it in ID view

--- PhoneAsId/www/js/controllers.js
+++ PhoneAsId/www/js/controllers.js
@@ -1,69 +1,100 @@
 angular.module('starter.controllers', ['ionic', 'starter.controllers'])
 
-.controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', '$http', 'serverName', function($scope, $ionicModal, $timeout, $http, serverName) {
+  .controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', '$http', 'serverName', function ($scope, $ionicModal, $timeout, $http, serverName) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-    var url = serverName + "student";
-    $http.get(url).success( function(response) {
-      alert(response[0].firstName);
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
     });
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-}])
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+      var url = serverName + "student";
+      $http.get(url).success(function (response) {
+        alert(response[0].firstName);
+      });
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  }])
 
 
+  .controller('LoginCtrl', function ($scope) {
 
-.controller('LoginCtrl', function($scope) {
-
-})
+  })
 
 
-.controller('IdCtrl', function($scope) {
+  .controller('IdCtrl', function ($scope) {
 
-})
+  })
 
-.controller('IdCtrl', function($scope,$window,$state) {
-  $scope.screenOrientation = screen.orientation.type;
+  .controller('IdCtrl', function ($scope, $window, $state, $http) {
 
-  //changes the template view used when the phones orientation changes
-  $window.addEventListener("orientationchange", function(){
-    console.log(screen.orientation.type); //"portrait-primary" or "landscape-secondary"
+    var studentid = '38901062';
+    $scope.studentInfo = "";
+
+    //TODO move get request to login activity and pass id after successful authentication
+    $http.get('https://myconcordiaid.azurewebsites.net/api/student/' + studentid)
+      .success(function (data) {
+        console.log('data success');
+        console.log(data);
+
+        $scope.studentInfo = data;
+
+        $scope.netname = data.netname;
+        $scope.valid = data.valid;
+        $scope.pending = data.pending;
+        $scope.updatepicture = data.updatepicture;
+
+        //variables to be displayed on id
+        $scope.profilepicture = data.profilepicture;
+        $scope.firstname = data.firstname;
+        $scope.lastname = data.lastname;
+        $scope.dob = data.dob;
+        $scope.id = data.id;
+        $scope.ugradstatus = data.ugradstatus;
+        $scope.expiredate = data.expiredate;
+      })
+      .error(function (data) {
+        console.log('data error');
+      });
+
     $scope.screenOrientation = screen.orientation.type;
-    $state.reload();
+
+    //changes the template view used when the phones orientation changes
+    $window.addEventListener("orientationchange", function () {
+      console.log(screen.orientation.type); //"portrait-primary" or "landscape-secondary"
+      $scope.screenOrientation = screen.orientation.type;
+      $state.reload();
+    });
+
+
   });
-});
 
 
