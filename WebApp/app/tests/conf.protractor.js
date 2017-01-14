7e796ce4a4a64cea4692c89b43160804c4e8029a
francisct <franciscotetremblay@gmail.com>
2017-01-13 19:01:43 -0500

    #57 first test written. It tests the search function and the validationModal information display

--- WebApp/app/tests/conf.protractor.js
+++ WebApp/app/tests/conf.protractor.js
@@ -1,8 +1,14 @@
-/**
- * Created by franc on 1/13/2017.
- */
+var env = require('./environment.protractor.js');
+var https = require('https');
 
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['search.test.js']
+    framework: 'jasmine',
+    seleniumAddress: env.seleniumAddress,
+    specs: [
+        'search.test.js'
+    ],
+    onPrepare: function(){
+        https.get(env.urlBackendInit).end(function(err, resp) {
+        });
+    }
 };
--- WebApp/app/tests/search.test.js
+++ WebApp/app/tests/search.test.js
@@ -1,13 +1,36 @@
-﻿var input = "61923560";
+﻿
 
 describe('student validation center student search', function() {
     it('should return a list of student corresponding to the search criteria', function() {
         browser.get('https://concordiaidclient.netlify.com/WebApp/app/review');
-
-        element(by.id('searchterm')).sendKeys(input);
+        var EC = protractor.ExpectedConditions;
+        var id = "61923560";
+        element(by.id('searchterm')).sendKeys(id);
         element(by.id('search')).click();
 
-        var searchResults = element.all(by.id('searchResults'));
-        expect(searchResults.count()).toEqual(1);
+
+        var searchResultsUl = element(by.id('searchResults'));
+        browser.wait(EC.presenceOf(searchResultsUl), 5000).then(function(){
+            var searchResults = element.all(by.repeater('result in search.results'));
+            expect(searchResults.count()).toEqual(1);
+
+            //open the modal
+            searchResults.each(function (el){
+                el.click();
+            });
+
+            var spanWithId = element(by.xpath('//div[contains(@class, "validationModal")]//span[contains(., "61923560")]'));
+            browser.wait(EC.presenceOf(spanWithId), 5000).then(function(){
+                expect(spanWithId.getText()).toEqual(id);
+            }, function (error) {
+                console.log("validationModal did not display");
+                expect(true).toBe(false);
+            });
+        }, function (error) {
+            console.log("searchResults did not display");
+            expect(false).toBe(true);
+        });
+
+
     });
 });
